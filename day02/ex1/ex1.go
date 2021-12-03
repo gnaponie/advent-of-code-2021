533c42cc7ac9c87d0cda3acdee32f4791c8b3096
Giulia Naponiello <gnaponie@redhat.com>
2021-12-03 15:53:50 +0100

    Day 2, ex 1
    Signed-off-by: Giulia Naponiello <gnaponie@redhat.com>

---
+++ day02/ex1/ex1.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "log"
+    "os"
+    "strconv"
+    "strings"
+)
+
+func main() {
+    // horizontal position
+    var hor_pos, depth = 0, 0
+
+    // open file
+    file, err := os.Open("../input.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        line := strings.Split(scanner.Text(), " ")
+        num, err := strconv.Atoi(line[1])
+        if err != nil {
+            log.Fatal(err)
+        }
+        if line[0] == "forward" {
+            hor_pos += num 
+        } else if line[0] == "down" {
+            depth += num
+        } else if line[0] == "up" {
+            depth -= num
+        }
+    }
+
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println(hor_pos * depth)
+}
